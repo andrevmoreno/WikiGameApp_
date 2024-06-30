b076e7ad9674caba0b2899e645114cca5f8cf3c0
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-06-30 19:11:13 -0400

    Update Responsive

---
+++ WikiGame/Models/Views/SystemPoints.swift
@@ -0,0 +1,18 @@
+//
+//  SystemPoints.swift
+//  WikiGame
+//
+//  Created by Andrea Moreno on 30-06-24.
+//
+
+import SwiftUI
+
+struct SystemPoints: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SystemPoints()
+}
